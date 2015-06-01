34b44c1b2e0f837e66b1b7b89ef468fbcb19626b
willem_vermin <willem_vermin@0efbc837-98b3-4d2d-9738-0fe6a19c6a65>
2015-06-01 11:00:33 +0000

    copy findent findent-1.6

---
+++ trunk/findent-1.6/findent.h
@@ -0,0 +1,25 @@
+#ifndef FINDENT_H
+#define FINDENT_H
+using namespace std;
+void lexer_init();
+int yyparse();
+void yyerror(const char *c);
+extern "C" int yylex();
+void lexer_pop();
+void lexer_push(const std::string s, int start);
+void lexer_set(const string s, int start);
+
+extern int  lexer_position;
+extern bool stlabel_found;
+extern struct rstruct 
+{
+   int kind;
+   string name;
+} rprop;
+
+#define O(x) {cerr << " [" << x << "] ";}
+#define Y O(yytext)
+// macro to activate or deactivate debug output:
+//#define D(x) {cerr << "WD: "<< __FILE__ <<":"<<__LINE__<<" "; x; cerr << endl; }
+#define D(x) ;
+#endif
---
+++ trunk/findent-1.6/version.h
@@ -0,0 +1,4 @@
+#ifndef VERSION_H
+#define VERSION_H
+#define VERSION "1.6.5"
+#endif
