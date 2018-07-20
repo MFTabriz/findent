b2f44472b90281b1d0cedfb05968958b784dbbd1
willem vermin <1525075+wvermin@users.noreply.github.com>
2018-07-20 15:03:22 +0000

    see Changelog

--- findent-devel/src/fortranline.cpp
+++ findent-devel/src/fortranline.cpp
@@ -1,24 +1,26 @@
 #include "fortranline.h"
 int fortranline::format;
+int fortranline::line_length;
+bool fortranline::gnu_format;
 void fortranline::print()
 {
    std::cout << "format:" << format2txt() << std::endl;
    std::cout << "orig_line: [" << orig_line << "]" << std::endl;
-   if(has_trim)
+   if(have_trim)
       std::cout << "trim_line: [" << trim_line << "]" << std::endl;
-   if(has_ltrim)
+   if(have_ltrim)
       std::cout << "ltrim_line: [" << ltrim_line << "]" << std::endl;
-   if(has_rtrim)
+   if(have_rtrim)
       std::cout << "rtrim_line: [" << rtrim_line << "]" << std::endl;
-   if(has_first)
+   if(have_first)
       std::cout << "firstchar: [" << first << "]" << std::endl;
-   if(has_last)
+   if(have_last)
       std::cout << "lastchar: [" << last << "]" << std::endl;
-   if(has_first2)
+   if(have_first2)
       std::cout << "first2chars: [" << first2 << "]" << std::endl;
-   if(has_ltab2sp)
+   if(have_ltab2sp)
       std::cout << "ltab2sp: [" << ltab2sp_line << "]" << std::endl;
-   if(has_scanfixpre)
+   if(have_scanfixpre)
       std::cout << "scanfixpre: " << Scanfixpre << std::endl;
 }
 
