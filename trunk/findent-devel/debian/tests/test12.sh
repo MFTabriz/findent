87bd4dda09d97f45a87f9bd59caf3a04115ae1db
willem_vermin <willem_vermin@0efbc837-98b3-4d2d-9738-0fe6a19c6a65>
2018-10-28 15:39:31 +0000

    devel: version 3.0.0~pre1

--- trunk/findent-devel/debian/tests/test12.sh
+++ trunk/findent-devel/debian/tests/test12.sh
@@ -6,9 +6,9 @@ else
 fi
 rc=0
 cat << eof > prog
-        program prog
+      program prog
 
-          continue
+      continue
 !  comment
 eof
 cat << eof > expect
@@ -22,12 +22,12 @@ rc=`expr $rc + $?`
 rc=`expr $rc + $?`
 
 cat << eof > prog
-  program prog
-  include "a.inc"
-  include 'b.inc'
+program prog
+include "a.inc"
+include 'b.inc'
 ?? include 'c.inc'
 ??include "d.inc"
-  #include "e.inc"
+#include "e.inc"
 # include <f.inc>
     end
 eof
@@ -44,14 +44,14 @@ eof
 rc=`expr $rc + $?`
 
 cat << eof > prog
-      program prog
-      include "a.inc"
-      include 'b.inc'
+       program prog
+       include "a.inc"
+       include 'b.inc'
 ?? include 'c.inc'
 ??include "d.inc"
-  #include "e.inc"
+#include "e.inc"
 # include <f.inc>
-        end
+ end
 eof
 cat << eof > expect
 inc a.inc
@@ -64,6 +64,63 @@ eof
 
 ../doit "--deps" "-ifixed" ""
 rc=`expr $rc + $?`
+
+cat << eof > main.f90
+module mm
+integer i
+end
+program main
+use mymod
+use mm
+include "file.inc"
+#include "one.inc"
+continue
+end
+eof
+cat << eof > sub.f90
+module mymod
+integer x
+contains
+subroutine sub
+continue
+end subroutine
+end module
+module mymod1
+interface
+real module function f(a)
+real a
+end function
+end interface
+end module
+eof
+cat << eof > sub1.f90
+submodule (mymod1) mymod2
+contains
+module procedure f
+f = 10.0
+end procedure
+end submodule
+eof
+cat << eof |sort >  expect
+sub1.lo: sub.lo
+main.lo: sub.lo
+main.lo: file.inc one.inc
+eof
+touch file.inc one.inc
+# remove '\r' to facilitate dos executable
+$FINDENT --makefdeps | tr -d '\r' > makefdeps.sh
+chmod +x makefdeps.sh
+./makefdeps.sh -s .lo main.f90 sub.f90 sub1.f90 | tr -d '\r' | sort > result
+if cmp -s expect result ; then
+   echo "--makefdeps : OK"
+else
+   echo "--makefdeps : NOT OK"
+   echo "expected:"
+   cat expect
+   echo "got:"
+   cat result
+   rc=`expr $rc + 1`
+fi
 
 . ../postlude
 exit $rc
---
+++ trunk/findent-devel/maketest/sub2.f90
@@ -0,0 +1,6 @@
+   submodule (mymod) mymod2
+   contains
+      module procedure sub
+   include 'sub.inc'
+      end procedure
+   end submodule
